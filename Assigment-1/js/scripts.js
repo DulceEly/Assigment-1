93d2eb8c255aa7b5050f84a8bdf1671ebf82396d
DulceEly <icc.dulce.caldino@gmail.com>
2022-03-02 00:57:13 -0800

    init commit

---
+++ Assigment-1/js/scripts.js
@@ -0,0 +1,119 @@
+
+// STEP 1
+
+/* let some_month;
+function the Month();
+let current-month;
+let summer_month;
+let MyLibrary-function
+
+to 
+
+let someMonth;
+function theMonth();
+let currentMonth;
+let summerMonth;
+let myLibraryFunction;
+ */
+
+
+// STEP 2
+
+
+/* 2.5 				    // numeric literal 
+'Hello world!' 			// string literal 
+true 				    // Boolean literal 
+null 				    // literal null value 
+{x:7, y:8} 			    // Object literal 
+[1,2,3] 			    // Array literal 
+function(y){return y*y;} 	// function literal
+
+ */
+// STEP 3
+
+/* let someMonth = 2 * (2022 / 2220) + 2;
+ */
+
+// STEP 4
+
+/* var <firstName>;
+var <lastName>;
+var <address>;
+var <city>;
+var <state>;
+var <zipCode>;
+var <yourAge>;
+var <referralSource>;
+var <mayWeContactYou>;
+ */
+
+// STEP 5
+
+var firstName; 
+firstName = 'Dulce'; // assigned a string value
+console.log(firstName); // access a variable
+
+var lastName; 
+lastName = 'DeNovellis'; // assigned a string value
+console.log(lastName); 
+
+var city; 
+city = 'San Diego' // assigned a string value
+console.log(city); 
+
+
+// STEP 6
+//Add a number and a string and display the coerced result in the browser’s console window. 
+
+/* var yourAge = 40; 
+console.log(`My age is ${yourAge}.`+ );
+
+var firstName; 
+firstName = 'Dulce';
+console.log(`Hello, my name is (${firstName}.`));
+
+ */
+
+// STEP 7
+
+
+
+// STEP 8
+
+// STEP 9
+
+
+// STEP 10
+
+
+// STEP 11
+
+
+// STEP 12
+
+
+// STEP 13
+
+
+// STEP 14
+
+
+// STEP 15
+
+
+
+// STEP 16
+
+
+// STEP 17
+
+
+// STEP 18
+
+
+// STEP 19
+
+
+// STEP 20
+
+
